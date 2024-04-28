ef844b1ca6bd8a1dd7185fbc33070631f5546831
FelixWang <wang.qiang@oalola.chat>
2024-04-28 18:49:34 +0800

    ios 1

--- ios/Classes/FlutterWechatQrcodePlugin.m
+++ ios/Classes/FlutterWechatQrcodePlugin.m
@@ -1,20 +1,26 @@
 #import "FlutterWechatQrcodePlugin.h"
 
 @implementation FlutterWechatQrcodePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_wechat_qrcode"
-            binaryMessenger:[registrar messenger]];
-  FlutterWechatQrcodePlugin* instance = [[FlutterWechatQrcodePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
++ (void)registerWithRegistrar:(NSObject <FlutterPluginRegistrar> *)registrar {
+    FlutterMethodChannel *channel = [FlutterMethodChannel
+            methodChannelWithName:@"flutter_wechat_qrcode"
+                  binaryMessenger:[registrar messenger]];
+    FlutterWechatQrcodePlugin *instance = [[FlutterWechatQrcodePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"scanImage" isEqualToString:call.method]) {
+
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"scanCamera" isEqualToString:call.method]) {
+
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
--- ios/flutter_wechat_qrcode.podspec
+++ ios/flutter_wechat_qrcode.podspec
@@ -16,6 +16,7 @@ A new Flutter project.
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'WeChatQRCodeScanner'
   s.platform = :ios, '11.0'
 
   # Flutter.framework does not contain a i386 slice.
